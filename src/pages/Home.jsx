79f6c8ca82cfcaab4f6964eb6ef5ad35884075b6
Chirag Agrawal <91793700+chirag9752@users.noreply.github.com>
2024-12-31 01:07:23 +0530

    small space fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useNavigate} from 'react-router-dom'
 
 const Home = () => {
-
   const navigate = useNavigate();
   const goToLoginHandler = () => {
       navigate("/login");
